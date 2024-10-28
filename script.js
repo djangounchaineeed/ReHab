0adc6078552032020be84827d2f80290ce332536
djangounchaineeed <misterwot@gmail.com>
2024-10-28 17:17:30 +0200

    my

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.header-burger').click(function (event) { 
+        $('.header-burger, .header-nav').toggleClass('active');
+        $('body'). toggleClass ('lock')
+    });
+});
